879d92cb037da1a518647e521ac873457d447fa0
Prathamesh Joshi <prathamesh1729@gmail.com>
2024-01-29 11:51:05 -0800

    feat(scrimba-prj-01): Implementing dark-mode

--- scrimba-prj-01/src/App.js
+++ scrimba-prj-01/src/App.js
@@ -1,13 +1,23 @@
+import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Main from './components/Main';
+// import Navbar from './components/Navbar';
+// import Main from './components/Main';
+import NavbarRemix from './components/NavbarRemix';
+import MainRemix from './components/MainRemix';
 
 
 export default function App() {
+
+    const [darkMode, setDarkMode] = React.useState(false);
+
+    function toggleDarkMode() {
+        setDarkMode(!darkMode);
+    };
+
     return (
-        <div>
-            <Navbar />
-            <Main />
+        <div className='container'>
+            <NavbarRemix darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+            <MainRemix darkMode={darkMode} />
         </div>
     )
 }
